6fea9d4137dea790f5f4d077f2a448fa195ced6d
Adrian Ross <adriantoddross@gmail.com>
2023-05-08 16:15:13 -0700

    Add layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,93 +1,27 @@
-import { useMutation, useQuery } from '@apollo/client'
-import { graphql } from 'lib/gql'
-import Link from 'next/link'
-import { useState } from 'react'
-import { initializeApollo } from '../lib/apollo'
+import Link from "next/link";
 
-const updateNameDocument = graphql(/* GraphQL */ `
-  mutation UpdateName($name: String!) {
-    updateName(name: $name) {
-      id
-      name
-      status
-    }
-  }
-`)
-
-const viewerDocument = graphql(/* GraphQL */ `
-  query Viewer {
-    viewer {
-      id
-      name
-      status
-    }
-  }
-`)
-
-const Index = () => {
-  const { data } = useQuery(viewerDocument)
-  const [newName, setNewName] = useState('')
-  const [updateNameMutation] = useMutation(updateNameDocument)
-
-  const onChangeName = () => {
-    updateNameMutation({
-      variables: {
-        name: newName,
-      },
-      // Follow apollo suggestion to update cache
-      //  https://www.apollographql.com/docs/angular/features/cache-updates/#update
-      update: (cache, mutationResult) => {
-        const { data } = mutationResult
-        if (!data) return // Cancel updating name in cache if no data is returned from mutation.
-        // Read the data from our cache for this query.
-        const result = cache.readQuery({
-          query: viewerDocument,
-        })
-
-        const newViewer = result ? { ...result.viewer } : null
-        // Add our comment from the mutation to the end.
-        // Write our data back to the cache.
-        if (newViewer) {
-          newViewer.name = data.updateName.name
-          cache.writeQuery({
-            query: viewerDocument,
-            data: { viewer: newViewer },
-          })
-        }
-      },
-    })
-  }
-
-  const viewer = data.viewer
-
-  return viewer ? (
-    <div>
-      You're signed in as {viewer.name} and you're {viewer.status}. Go to the{' '}
-      <Link href="/about">about</Link> page.
+export default function About() {
+  return (
+    <section>
       <div>
-        <input
-          type="text"
-          placeholder="your new name..."
-          onChange={(e) => setNewName(e.target.value)}
-        />
-        <input type="button" value="change" onClick={onChangeName} />
+        <h1>Yoga Poses</h1>
+        <p>View random yoga poses with the click of a button.</p>
       </div>
-    </div>
-  ) : null
+
+      <div>
+        <img src="#" />
+      </div>
+
+      <div>
+        <p>Text goes here</p>
+        <button type="button" onClick={() => console.log("Button clicked!")}>
+          Next
+        </button>
+      </div>
+    </section>
+  );
 }
 
-export async function getStaticProps() {
-  const apolloClient = initializeApollo()
-
-  await apolloClient.query({
-    query: viewerDocument,
-  })
-
-  return {
-    props: {
-      initialApolloState: apolloClient.cache.extract(),
-    },
-  }
-}
-
-export default Index
+/*
+Where do I put static images?
+*/
