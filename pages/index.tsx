321b59a82f26ab22fbfccf76ef20d2b40b758d66
Adrian Ross <adriantoddross@gmail.com>
2023-05-11 20:32:47 -0700

    Stub event handlers and logic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,20 @@
 import Link from "next/link";
+import { useState } from "react";
+
+const yogaPosesData = [
+  { src: "#", description: "1. Describing the pose" },
+  { src: "#", description: "2. History of the pose" },
+  { src: "#", description: "3. Context around the pose" },
+];
+
+const handleShowNextImage = () =>
+  console.log("Display next image and description");
+
+// Add React state initialized with yogaPosesData.
+
+// Add event handler
+// The state variable should be a number that gets incremented
+// If state is greater than 2, reset state to 0
 
 export default function About() {
   return (
@@ -14,7 +30,7 @@ export default function About() {
 
       <div>
         <p>Text goes here</p>
-        <button type="button" onClick={() => console.log("Button clicked!")}>
+        <button type="button" onClick={handleShowNextImage}>
           Next
         </button>
       </div>
