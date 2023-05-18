5a2dc934feebe0a32d7412420308aaffdc21bc45
Adrian Ross <adriantoddross@gmail.com>
2023-05-18 00:13:42 -0700

    Stub new layout

---
+++ pages/index2.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import Image from "next/image";
+import { yogaPosesData, showNextPose } from "util/yogaPosesData";
+
+export default function Home() {
+  return (
+    <div>
+      <div>
+        page template
+        <div>
+          container
+          <div>yoga poses compnent</div>
+        </div>
+      </div>
+    </div>
+  );
+}
