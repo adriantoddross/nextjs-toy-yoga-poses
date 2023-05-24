8171dee270cc6e70f0083ee560ab0a0f82e6aac9
Adrian Ross <adriantoddross@gmail.com>
2023-05-23 17:17:27 -0700

    Extract yoga poses component

---
+++ components/yogaPoses.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import Image from "next/image";
+import { yogaPosesData, showNextPose } from "util/yogaPosesData";
+
+export default function YogaPoses() {
+  const [currentPoseIndex, setPoseIndex] = useState(0);
+
+  const handleShowNextImage = () => {
+    setPoseIndex(showNextPose(currentPoseIndex, yogaPosesData));
+    return;
+  };
+
+  const { alt, description, src, title } = yogaPosesData[currentPoseIndex];
+
+  return (
+    <section>
+      <div>
+        <h1>Yoga Poses</h1>
+        <p>View random yoga poses with the click of a button.</p>
+      </div>
+
+      <div>
+        <img src={src} alt={alt} />
+      </div>
+
+      <section>
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <button type="button" onClick={handleShowNextImage}>
+          Show next pose
+        </button>
+      </section>
+    </section>
+  );
+}
