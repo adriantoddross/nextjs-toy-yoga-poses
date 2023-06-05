2275bb59fb73065a33f05fc5d78fd0e2f7f2687c
Adrian Ross <adriantoddross@gmail.com>
2023-06-05 13:12:17 -0700

    Add and test login and logout buttons with Jimmy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,12 @@
 import YogaPoses from "components/yogaPoses";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function Home() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
   return (
     <>
       <div className="min-h-full">
@@ -10,6 +16,23 @@ export default function Home() {
               Yoga Poses
             </h1>
             <p>View random yoga poses with the click of a button</p>
+            <div></div>
+            <div>
+              {user ? (
+                <a href="/api/auth/logout">Logout</a>
+              ) : (
+                <a href="/api/auth/login">Login</a>
+              )}
+            </div>
+            <div>
+              {user && (
+                <div>
+                  <img src={user.picture} alt={user.name} />
+                  <h2>{user.name}</h2>
+                  <p>{user.email}</p>
+                </div>
+              )}
+            </div>
           </div>
         </header>
         <main>
