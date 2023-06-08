845af4a23939101511b6b555e3a58c5d32530ac8
Adrian Ross <adriantoddross@gmail.com>
2023-06-07 17:25:10 -0700

    Use Header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,48 +1,12 @@
 import YogaPoses from "components/yogaPoses";
-import { useUser } from "@auth0/nextjs-auth0/client";
 import Link from "next/link";
+import Header from "components/header";
 
 export default function Home() {
-  const { user, error, isLoading } = useUser();
-
-  if (isLoading) return <div>Loading...</div>;
-
-  if (error) return <div>{error.message}</div>;
-
   return (
     <>
       <div className="min-h-full">
-        <header className="bg-white shadow">
-          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">
-              Yoga Poses
-            </h1>
-            <p>View random yoga poses with the click of a button</p>
-            <div>
-              {user ? (
-                <a href="/api/auth/logout">Logout</a>
-              ) : (
-                <ul>
-                  <li>
-                    <a href="/api/auth/login">Login</a>
-                  </li>
-                  <li>
-                    <Link href="/profile">Profile</Link>
-                  </li>
-                </ul>
-              )}
-            </div>
-            <div>
-              {user && (
-                <div>
-                  <img src={user.picture} alt={user.name} />
-                  <h2>{user.name}</h2>
-                  <p>{user.email}</p>
-                </div>
-              )}
-            </div>
-          </div>
-        </header>
+        <Header />
         <main>
           <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
