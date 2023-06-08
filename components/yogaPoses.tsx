3be3cf11e83c52cd18e7f3f38bde160dc01f87fc
Adrian Ross <adriantoddross@gmail.com>
2023-06-07 17:14:05 -0700

    Query data from Hasura!; Show title & subtitle

--- components/yogaPoses.tsx
+++ components/yogaPoses.tsx
@@ -1,36 +1,54 @@
 import { useState } from "react";
 import Image from "next/image";
 import { yogaPosesData, showNextPose } from "util/yogaPosesData";
+import { gql, useQuery } from "@apollo/client";
 
 export default function YogaPoses() {
+  const GET_POSES = gql`
+    query GetPoses {
+      poses {
+        id
+        created_at
+        image
+        subtitle
+        title
+        difficulty
+      }
+    }
+  `;
+
+  const { loading, error: QueryError, data } = useQuery(GET_POSES);
+
   const [currentPoseIndex, setPoseIndex] = useState(0);
 
   const handleShowNextImage = () => {
-    // Replace yogaPosesData with data from query
-    setPoseIndex(showNextPose(currentPoseIndex, yogaPosesData));
+    setPoseIndex(showNextPose(currentPoseIndex, data?.poses));
     return;
   };
 
-  const { alt, description, src, title } = yogaPosesData[currentPoseIndex];
-
   return (
     <section>
-      <div>
+      {/* <div>
         <img src={src} alt={alt} />
-      </div>
+      </div> */}
 
-      {/* Show this <section> if data.poses is truthy */}
-      <section className="flex flex-col space-y-2">
-        <h2 className="text-xl font-bold pt-2">{title}</h2>
-        <p className="">{description}</p>
-        <button
-          type="button"
-          onClick={handleShowNextImage}
-          className="border p-2"
-        >
-          Show next pose
-        </button>
-      </section>
+      {data?.poses && (
+        <section className="flex flex-col space-y-2">
+          <h2 className="text-xl font-bold pt-2">
+            {data?.poses[currentPoseIndex].title}
+          </h2>
+          {data?.poses[currentPoseIndex].subtitle && (
+            <p className="">{data?.poses[currentPoseIndex].subtitle}</p>
+          )}
+          <button
+            type="button"
+            onClick={handleShowNextImage}
+            className="border p-2"
+          >
+            Show next pose
+          </button>
+        </section>
+      )}
     </section>
   );
 }
