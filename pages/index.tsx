097a0ae583d6441f33222968395c1c065d9fa8c9
Adrian Ross <adriantoddross@gmail.com>
2023-06-07 17:32:35 -0700

    Use Layout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,20 +3,5 @@ import Link from "next/link";
 import Header from "components/header";
 
 export default function Home() {
-  return (
-    <>
-      <div className="min-h-full">
-        <Header />
-        <main>
-          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-              <div className="mx-auto max-w-3xl">
-                <YogaPoses />
-              </div>
-            </div>
-          </div>
-        </main>
-      </div>
-    </>
-  );
+  return <YogaPoses />;
 }
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -9,22 +9,16 @@ export default function Home() {
   if (error) return <div>{error.message}</div>;
 
   return (
-    <main>
-      <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div className="mx-auto max-w-3xl">
-            {user ? (
-              <div>
-                <img src={user.picture} alt={user.name} />
-                <h2>{user.name}</h2>
-                <p>{user.email}</p>
-              </div>
-            ) : (
-              <div>Error</div>
-            )}
-          </div>
+    <div className="mx-auto max-w-3xl">
+      {user ? (
+        <div>
+          <img src={user.picture} alt={user.name} />
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
         </div>
-      </div>
-    </main>
+      ) : (
+        <div>Error</div>
+      )}
+    </div>
   );
 }
