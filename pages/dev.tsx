1e7e2915739364311b520a12f8161f4fca8b1398
Adrian Ross <adriantoddross@gmail.com>
2023-06-16 15:01:25 -0700

    Update layout; comment out pseudocode for now

--- pages/dev.tsx
+++ pages/dev.tsx
@@ -2,7 +2,7 @@ import YogaPoses from "components/yogaPoses";
 import Link from "next/link";
 import Header from "components/header";
 import { useState } from "react";
-// import redux toolkit
+import Sidebar from "components/Sidebar";
 
 export default function Home({ exercises }) {
   const [showPoses, setShowPoses] = useState(false); // useState
@@ -18,20 +18,23 @@ export default function Home({ exercises }) {
   return (
     <div>
       <Sidebar
-        exercises={exercises}
-        onExerciseSelected={handleExerciseSelected}
+      // exercises={exercises}
+      // onExerciseSelected={handleExerciseSelected}
       />
-      {showPoses ? (
+      <main>
+        Yoga cards
+        {/* {showPoses ? (
         <YogaPosesCards exercises={filteredExercises} />
       ) : (
         <div>Select an exercise to get started!</div>
-      )}
+      )} */}
+      </main>
     </div>
   );
 }
 
-export const getServerSideProps = async () => {
-  // graphql api call
-  //  const exercises =  api call results
-  return { exercises };
-};
+// export const getServerSideProps = async () => {
+//   // graphql api call
+//   //  const exercises =  api call results
+//   return { exercises };
+// };
