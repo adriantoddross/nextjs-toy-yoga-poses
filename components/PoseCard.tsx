6ebf35e830cf19a286f3ba6babde5ee5ab160883
Adrian Ross <adriantoddross@gmail.com>
2023-06-21 09:45:21 -0700

    Add types for Poses

--- components/PoseCard.tsx
+++ components/PoseCard.tsx
@@ -4,10 +4,23 @@ import Header from "components/header";
 import { useState } from "react";
 import Sidebar from "components/Sidebar";
 
-export default function PoseCard({ title }) {
+type Pose = {
+  id: number;
+  title: string;
+  subtitle: string;
+  image_url: string;
+};
+
+type Props = {
+  pose: Pose;
+};
+export default function PoseCard({ pose }: Props) {
+  const { title, subtitle, image_url } = pose;
   return (
     <div>
       <p>{title}</p>
+      <p>{subtitle}</p>
+      <img src={image_url} />
     </div>
   );
 }
