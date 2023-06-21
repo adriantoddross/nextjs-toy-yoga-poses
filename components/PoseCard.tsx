2d814300326ce93983c78f5f160ddfb448a30f3f
Adrian Ross <adriantoddross@gmail.com>
2023-06-21 13:35:09 -0700

    Extract Pose type

--- components/PoseCard.tsx
+++ components/PoseCard.tsx
@@ -1,19 +1,9 @@
-import YogaPoses from "components/yogaPoses";
-import Link from "next/link";
-import Header from "components/header";
-import { useState } from "react";
-import Sidebar from "components/Sidebar";
-
-type Pose = {
-  id: number;
-  title: string;
-  subtitle: string;
-  image_url: string;
-};
+import { Pose } from "lib/typeDefs/types";
 
 type Props = {
   pose: Pose;
 };
+
 export default function PoseCard({ pose }: Props) {
   const { title, subtitle, image_url } = pose;
   return (
---
+++ lib/typeDefs/types.ts
@@ -0,0 +1,8 @@
+type Pose = {
+  id: number;
+  title: string;
+  subtitle: string;
+  image_url: string;
+};
+
+export type { Pose };
