edbf3dba1cbdf48a611cc4681540011c04dfd60a
Adrian Ross <adriantoddross@gmail.com>
2023-06-21 17:14:01 -0700

    Add pseudocode for callback

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,12 +7,19 @@ export default function Sidebar() {
   // { exercises, onExerciseSelected }
   // onExerciseSelected helper function to filter & display YogaPosesCards
 
+  const handleFilterOptions = (e) => console.log(e);
+  // This callback will filter out the exercises based on the filters selected
+
   const { data } = useQuery(GET_EXERCISES);
 
   return (
     <div>
       <Header />
-      <CheckboxList exercises={data?.exercise} />
+      <CheckboxList
+        exercises={data?.exercise}
+        handleExerciseSelected={handleFilterOptions}
+        // handleExerciseSelected should be renamed for clarity
+      />
     </div>
   );
 }
