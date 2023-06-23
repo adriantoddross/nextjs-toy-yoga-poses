82cb29e1cb35379e033eb3e47b4f5bd43e465f5f
Adrian Ross <adriantoddross@gmail.com>
2023-06-23 14:59:39 -0700

    Update nav items

--- pages/example.tsx
+++ pages/example.tsx
@@ -32,12 +32,15 @@ import {
 } from "@heroicons/react/20/solid";
 
 const navigation = [
-  { name: "Dashboard", href: "#", icon: HomeIcon, current: true },
-  { name: "Team", href: "#", icon: UsersIcon, current: false },
-  { name: "Projects", href: "#", icon: FolderIcon, current: false },
-  { name: "Calendar", href: "#", icon: CalendarIcon, current: false },
-  { name: "Documents", href: "#", icon: DocumentDuplicateIcon, current: false },
-  { name: "Reports", href: "#", icon: ChartPieIcon, current: false },
+  { name: "Home", href: "/", icon: HomeIcon, current: true },
+  { name: "Login", href: "/api/auth/login", icon: UsersIcon, current: false },
+  {
+    name: "Logout",
+    href: "/api/auth/logout",
+    icon: FolderIcon,
+    current: false,
+  },
+  { name: "Profile", href: "/profile", icon: CalendarIcon, current: false },
 ];
 const teams = [
   { id: 1, name: "Heroicons", href: "#", initial: "H", current: false },
