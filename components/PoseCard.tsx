e059ee14a2887179015a8bc9cc16f8b2ef39d305
Adrian Ross <adriantoddross@gmail.com>
2023-06-29 15:29:30 -0700

    Style PoseCard

--- components/PoseCard.tsx
+++ components/PoseCard.tsx
@@ -1,16 +1,52 @@
 import { Pose } from "lib/typeDefs/types";
+import { HeartIcon } from "@heroicons/react/24/outline";
+import { HeartIcon as SolidHeartIcon } from "@heroicons/react/20/solid";
 
 type Props = {
   pose: Pose;
+  isFavorited?: boolean;
 };
 
-export default function PoseCard({ pose }: Props) {
+export default function PoseCard2({ pose, isFavorited }: Props) {
   const { title, subtitle, image_url } = pose;
+
+  const toggleFavorite = () => console.log(`Favoriting ${title} pose`);
   return (
     <div>
-      <p>{title}</p>
-      <p>{subtitle}</p>
-      <img src={image_url} />
+      <div className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
+        <img
+          src={image_url}
+          alt=""
+          className="pointer-events-none object-cover group-hover:opacity-75"
+        />
+        <button
+          type="button"
+          className="absolute top-2 left-2 focus:outline-none"
+        >
+          <span className="sr-only">Favorite</span>
+          {isFavorited ? (
+            <SolidHeartIcon
+              className="h-5 w-5"
+              aria-hidden="true"
+              onClick={toggleFavorite}
+            />
+          ) : (
+            <HeartIcon
+              className="h-5 w-5"
+              aria-hidden="true"
+              onClick={toggleFavorite}
+            />
+          )}
+        </button>
+      </div>
+      <div className="mt-6">
+        <p className="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">
+          {title}
+        </p>
+      </div>
+      <p className="pointer-events-none block text-sm font-medium text-gray-500">
+        {subtitle}
+      </p>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -125,11 +125,27 @@ export default function Example() {
 
         <main className="py-10">
           <div className="px-4 sm:px-6 lg:px-8">
-            {showPoses
-              ? data?.poses.map((pose: Pose) => (
-                  <PoseCard key={pose.id} pose={pose} />
-                ))
-              : "Select an exercise to see your recommended yoga poses"}
+            <section>
+              <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                Yoga Poses
+              </h2>
+              <p className="mt-6 text-lg leading-8 text-gray-600">
+                Use the filters on the sidebar to find yoga poses that will
+                improve your workouts!
+              </p>
+            </section>
+            <ul
+              role="list"
+              className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8 mt-6"
+            >
+              {showPoses
+                ? data?.poses.map((pose: Pose) => (
+                    <li key={pose.title} className="relative">
+                      <PoseCard key={pose.id} pose={pose} />
+                    </li>
+                  ))
+                : "Select an exercise to see your recommended yoga poses"}
+            </ul>
           </div>
         </main>
       </div>
