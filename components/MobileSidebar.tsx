31e61f9ca4bca5bf3136752370b431802245ec31
Adrian Ross <adriantoddross@gmail.com>
2023-06-30 14:24:11 -0700

    Refactor mobile sidebar with Dave

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -4,11 +4,21 @@ import GET_EXERCISES from "lib/gql/queryDefs/getExercises";
 import classNames from "util/classnames";
 import navigation from "util/navigationLinks";
 import { NavigationProps } from "lib/typeDefs/types";
+import { Transition, Dialog } from "@headlessui/react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import { Fragment } from "react";
+
+type Props = {
+  sidebarOpen: boolean;
+  setSidebarOpen: (bool: boolean) => void;
+} & NavigationProps;
 
 export default function MobileSidebar({
   handleFiltersSelected,
   filteredExercises,
-}: NavigationProps) {
+  sidebarOpen,
+  setSidebarOpen,
+}: Props) {
   const handleFilterOptions = (id: number, checked: boolean) => {
     let updatedFilteredExercises = [...filteredExercises];
 
@@ -25,64 +35,123 @@ export default function MobileSidebar({
   const { data: allExercises } = useQuery(GET_EXERCISES);
 
   return (
-    <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 px-6 pb-4">
-      <div className="flex h-16 shrink-0 items-center">
-        <img
-          className="h-8 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=white"
-          alt="Your Company"
-        />
-      </div>
-      <nav className="flex flex-1 flex-col">
-        <ul role="list" className="flex flex-1 flex-col gap-y-7">
-          <li>
-            <ul role="list" className="-mx-2 space-y-1">
-              {navigation.map((item) => (
-                <li key={item.name}>
-                  <a
-                    href={item.href}
-                    className={classNames(
-                      item.current
-                        ? "bg-indigo-700 text-white"
-                        : "text-indigo-200 hover:text-white hover:bg-indigo-700",
-                      "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                    )}
+    <Transition.Root show={sidebarOpen} as={Fragment}>
+      <Dialog
+        as="div"
+        className="relative z-50 lg:hidden"
+        onClose={setSidebarOpen}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="transition-opacity ease-linear duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="transition-opacity ease-linear duration-300"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-900/80" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 flex">
+          <Transition.Child
+            as={Fragment}
+            enter="transition ease-in-out duration-300 transform"
+            enterFrom="-translate-x-full"
+            enterTo="translate-x-0"
+            leave="transition ease-in-out duration-300 transform"
+            leaveFrom="translate-x-0"
+            leaveTo="-translate-x-full"
+          >
+            <Dialog.Panel className="relative mr-16 flex w-full max-w-xs flex-1">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-in-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in-out duration-300"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <div className="absolute left-full top-0 flex w-16 justify-center pt-5">
+                  <button
+                    type="button"
+                    className="-m-2.5 p-2.5"
+                    onClick={() => setSidebarOpen(false)}
                   >
-                    <item.icon
-                      className={classNames(
-                        item.current
-                          ? "text-white"
-                          : "text-indigo-200 group-hover:text-white",
-                        "h-6 w-6 shrink-0"
-                      )}
+                    <span className="sr-only">Close sidebar</span>
+                    <XMarkIcon
+                      className="h-6 w-6 text-white"
                       aria-hidden="true"
                     />
-                    {item.name}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </li>
-          {filteredExercises && (
-            <li>
-              <div className="text-sm font-bold leading-6 text-indigo-200">
-                Filters
+                  </button>
+                </div>
+              </Transition.Child>
+              <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 px-6 pb-4">
+                <div className="flex h-16 shrink-0 items-center">
+                  <img
+                    className="h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=white"
+                    alt="Your Company"
+                  />
+                </div>
+                <nav className="flex flex-1 flex-col">
+                  <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                    <li>
+                      <ul role="list" className="-mx-2 space-y-1">
+                        {navigation.map((item) => (
+                          <li key={item.name}>
+                            <a
+                              href={item.href}
+                              className={classNames(
+                                item.current
+                                  ? "bg-indigo-700 text-white"
+                                  : "text-indigo-200 hover:text-white hover:bg-indigo-700",
+                                "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                              )}
+                            >
+                              <item.icon
+                                className={classNames(
+                                  item.current
+                                    ? "text-white"
+                                    : "text-indigo-200 group-hover:text-white",
+                                  "h-6 w-6 shrink-0"
+                                )}
+                                aria-hidden="true"
+                              />
+                              {item.name}
+                            </a>
+                          </li>
+                        ))}
+                      </ul>
+                    </li>
+                    {filteredExercises && (
+                      <li>
+                        <div className="text-sm font-bold leading-6 text-indigo-200">
+                          Filters
+                        </div>
+                        <fieldset>
+                          <legend className="text-xs font-semibold leading-6 text-indigo-200">
+                            By exercise
+                          </legend>
+                          <div>
+                            {/* <button type="button">Reset</button> */}
+                          </div>
+                          <CheckboxList
+                            exercises={allExercises?.exercise}
+                            handleExerciseSelected={handleFilterOptions}
+                            filteredExercises={filteredExercises}
+                          />
+                        </fieldset>
+                      </li>
+                    )}
+                  </ul>
+                </nav>
               </div>
-              <fieldset>
-                <legend className="text-xs font-semibold leading-6 text-indigo-200">
-                  By exercise
-                </legend>
-                <div>{/* <button type="button">Reset</button> */}</div>
-                <CheckboxList
-                  exercises={allExercises?.exercise}
-                  handleExerciseSelected={handleFilterOptions}
-                  filteredExercises={filteredExercises}
-                />
-              </fieldset>
-            </li>
-          )}
-        </ul>
-      </nav>
-    </div>
+            </Dialog.Panel>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition.Root>
   );
 }
