c9ea834dd700ad9caac391e9215f81777f3d3046
Adrian Ross <adriantoddross@gmail.com>
2023-07-03 15:59:56 -0700

    Pass basic assertion; fails on using gql from ApolloClient

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,35 +1,68 @@
 import { render } from "@testing-library/react";
-import Example from "../pages";
-import gql from "graphql-tag";
+import Home from "../pages";
+import { MockedProvider as MockedProviderBroken } from "@apollo/client/testing";
+// import { gql } from "@apollo/client";
+// import { UserProvider } from "@auth0/nextjs-auth0/client";
+// import { Exercise } from "lib/typeDefs/types";
 
-jest.mock("../lib/gql/queryDefs/getExercises", () => ({
-  GET_EXERCISES: () => gql`
-    query Get_Exercises {
-      exercise {
-        id
-        title
-      }
+const MockedProvider = MockedProviderBroken as any;
+
+const apolloClient = jest.requireActual("@apollo/client");
+
+jest.mock("@apollo/client", () => ({
+  ...apolloClient,
+  useQuery: (queryName: string, variables: { exerciseIds?: number[] }) => {
+    if (variables.exerciseIds) {
+      return {
+        data: [
+          {
+            id: 1,
+            title: "running",
+            __typename: "exercise",
+          },
+        ],
+      };
+    } else {
+      return {
+        data: [
+          {
+            id: 3,
+            title:
+              "War. War never changes. Neither does the classic Warrior One pose!",
+            subtitle: "Woman demonstrating warrior pose",
+            image_url:
+              "https://t20598560.p.clickup-attachments.com/t20598560/1ec3560e-0f56-409d-848f-54325e5e5be7/DownwardFacingDogAdhoMukahSvanasana_annotated2-918a8568b3af49d2add6d1949797a20f.webp",
+            __typename: "poses",
+          },
+        ],
+      };
     }
-  `,
+  },
 }));
 
-jest.mock("../lib/gql/queryDefs/getPoses", () => ({
-  GET_POSES: () => gql`
-    query GET_POSES($exerciseIds: [bigint!]) {
-      poses(where: { exercise: { _in: $exerciseIds } }) {
-        id
-        title
-        subtitle
-        image_url
-      }
-    }
-  `,
+jest.mock("@auth0/nextjs-auth0/client", () => ({
+  UserProvider: ({ children }) => "<div>{children}</div>",
+  useUser: () => ({
+    user: {
+      name: "adrienne",
+      image_url: "www.something.com/123",
+      email: "aross@thoughtbot.com",
+    },
+    error: null,
+    isLoading: false,
+  }),
 }));
 
 describe("Yoga app", () => {
   it("filters yoga poses", async () => {
-    const page = render(<Example />);
+    const page = render(
+      <MockedProvider mocks={[]}>
+        <Home />
+      </MockedProvider>
+    );
 
-    expect(page.getByText("Filters")).toBeInTheDocument();
+    // expect(page.getByText("Filters")).toBeInTheDocument();
+    await page.findByText("Filters");
+    expect(page).toMatchSnapshot();
   });
 });
