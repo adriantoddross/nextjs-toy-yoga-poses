38318953523390f27f8d52a89bd9686ad0fe1cb9
Adrian Ross <adriantoddross@gmail.com>
2023-07-06 15:59:40 -0700

    Handle missing props for pre-rendering

--- pages/poses/[id].tsx
+++ pages/poses/[id].tsx
@@ -8,18 +8,18 @@ export default function Pose() {
   const { query } = router;
   const { pathname: previousURL } = query;
 
-  const selectedPose = JSON.parse(query.pose as string);
-
-  console.log(query.pathname);
-
   return (
     <div>
       <Layout>
         <div className="px-4 sm:px-6 lg:px-8">
-          <PoseCard pose={selectedPose} />
+          {query.pose ? (
+            <PoseCard pose={JSON.parse(query.pose as string)} />
+          ) : (
+            <p>Sorry, there was an error loading this pose.</p>
+          )}
           <div className="mt-4">
             <Link
-              href={previousURL as string}
+              href={(previousURL as string) ?? "/"}
               className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Go back
