97c63056c50b5fabb5e72d77eaaecd4ff64fd66d
Adrian Ross <adriantoddross@gmail.com>
2023-07-07 15:47:27 -0700

    Fire add/remove favorites mutations and refetch favorites

--- components/PoseCard.tsx
+++ components/PoseCard.tsx
@@ -43,12 +43,10 @@ export default function PoseCard({ pose, isFavorited }: Props) {
   ) => {
     e.preventDefault();
     if (user) {
-      console.log("Favoriting pose: ", {
-        variables: { pose_id: id, user_id: user?.email },
+      addFavoritePose({
+        variables: { pose_id: id, user_id: user?.sub },
+        refetchQueries: ["getFavoritePoses"],
       });
-      // addFavoritePose({
-      //   variables: { pose_id: id, user_id: user?.email },
-      // });
     } else {
       throw new Error("Log in to favorite a pose");
     }
@@ -59,39 +57,40 @@ export default function PoseCard({ pose, isFavorited }: Props) {
   ) => {
     e.preventDefault();
     if (user) {
-      console.log("Removing favorite pose: ", {
-        variables: { pose_id: id, user_id: user?.email },
+      removeFavoritePose({
+        variables: { user_id: user.sub, pose_id: id },
+        refetchQueries: ["getFavoritePoses"],
       });
-      // removeFavoritePose({
-      //   variables: { user_pose_id: id },
-      // });
     } else {
       throw new Error("Log in to remove a favorited pose");
     }
   };
 
   return (
-    <Link
-      href={{
-        pathname: "/poses/[id]",
-        query: {
-          id: id,
-          pathname: pathname,
-          pose: JSON.stringify(pose),
-        },
-      }}
-    >
+    <div>
       {/* onClick, send query params to individual page! */}
       <div className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
-        <img
-          src={image_url}
-          alt=""
-          className="pointer-events-none object-cover group-hover:opacity-75"
-        />
+        <Link
+          href={{
+            pathname: "/poses/[id]",
+            query: {
+              id: id,
+              pathname: pathname,
+              pose: JSON.stringify(pose),
+            },
+          }}
+        >
+          <img
+            src={image_url}
+            alt=""
+            className="pointer-events-none object-cover group-hover:opacity-75"
+          />
+        </Link>
         {user ? (
           <FavoritePoseButton
             handleAddFavorite={handleAddFavoritePose}
             handleRemoveFavorite={handleRemoveFavoritePose}
+            isFavorited={isFavorited}
           />
         ) : (
           <LogInButton />
@@ -105,7 +104,7 @@ export default function PoseCard({ pose, isFavorited }: Props) {
       <p className="pointer-events-none block text-sm font-medium text-gray-500">
         {subtitle}
       </p>
-    </Link>
+    </div>
   );
 }
 
