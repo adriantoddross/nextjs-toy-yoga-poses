cadb25651eb8185d1080d4647d2b7838d30e98a0
Adrian Ross <adriantoddross@gmail.com>
2023-07-10 15:18:58 -0700

    Hide filters on profile page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,19 +1,31 @@
 import { useState } from "react";
 import { useUser } from "@auth0/nextjs-auth0/client";
+import { useQuery } from "@apollo/client";
 
 import Layout from "components/Layout";
+import GET_FAVORITE_POSES from "lib/gql/queryDefs/getFavoritePoses";
 
 export default function Example() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   const { user, error, isLoading } = useUser();
 
+  const { data: favoritePoses } = useQuery(GET_FAVORITE_POSES, {
+    variables: { user_id: user?.sub },
+    skip: !user,
+  });
+
+  // Query poses using pose_ids from favoritePoses
+  // Create new query in GraphiQL?
+  // Render results below
+  // Display placeholder message if there are no results
+
   if (isLoading) return <div>Loading...</div>;
 
   if (error) return <div>{error.message}</div>;
 
   return (
-    <Layout>
+    <Layout showFilters={false}>
       <div className="px-4 sm:px-6 lg:px-8">
         <div>
           {user ? (
