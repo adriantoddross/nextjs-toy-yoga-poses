4ec679fb67f418a26639c7fe3d48ded15964b973
Adrian Ross <adriantoddross@gmail.com>
2023-07-13 13:38:50 -0700

    Use loading spinner

--- components/NavigationMenu.tsx
+++ components/NavigationMenu.tsx
@@ -65,41 +65,42 @@ export default function NavigationMenu({
             ))}
           </ul>
         </li>
-        {showFilters && (
-          <li>
-            <div className="text-sm font-bold leading-6 text-indigo-200">
-              Filters
+        {showFilters &&
+          (exercisesLoading ? (
+            <div className="mt-4">
+              <LoadingSpinner color="text-white" />
             </div>
-            <fieldset>
-              <legend className="text-xs font-semibold leading-6 text-indigo-200">
-                By exercise
-              </legend>
-              <div className="mt-4 text-sm font-normal leading-6 text-indigo-200">
-                <button
-                  type="button"
-                  onClick={() => {
-                    setFilteredExercises(
-                      allExercises.exercise.map((exercise) => exercise.id)
-                    );
-                  }}
-                >
-                  Reset filters <span className="sr-only">(see all poses)</span>
-                </button>
+          ) : (
+            <li>
+              <div className="text-sm font-bold leading-6 text-indigo-200">
+                Filters
               </div>
-              {exercisesLoading ? (
-                <div className="mt-4">
-                  <LoadingSpinner color="text-white" />
+              <fieldset>
+                <legend className="text-xs font-semibold leading-6 text-indigo-200">
+                  By exercise
+                </legend>
+                <div className="mt-4 text-sm font-normal leading-6 text-indigo-200">
+                  <button
+                    type="button"
+                    onClick={() => {
+                      setFilteredExercises(
+                        allExercises.exercise.map((exercise) => exercise.id)
+                      );
+                    }}
+                  >
+                    Reset filters{" "}
+                    <span className="sr-only">(see all poses)</span>
+                  </button>
                 </div>
-              ) : (
+
                 <CheckboxList
                   exercises={allExercises?.exercise}
                   handleExerciseSelected={handleFilterOptions}
                   filteredExercises={filteredExercises}
                 />
-              )}
-            </fieldset>
-          </li>
-        )}
+              </fieldset>
+            </li>
+          ))}
       </ul>
     </nav>
   );
