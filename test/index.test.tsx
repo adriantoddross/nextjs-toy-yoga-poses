b7d770f133f6886cab2a4c18ee0631e966548a4e
Adrian Ross <adriantoddross@gmail.com>
2023-07-20 16:38:40 -0700

    Test fails when checking if pose has been filtered out!

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -4,12 +4,8 @@ import GET_EXERCISES from "lib/gql/queryDefs/getExercises";
 import GET_FAVORITE_POSES from "lib/gql/queryDefs/getFavoritePoses";
 import GET_POSES from "lib/gql/queryDefs/getPoses";
 import Home from "../pages";
-// import { UserProvider } from "@auth0/nextjs-auth0/client";
-// import { Exercise } from "lib/typeDefs/types";
 
 const MockedProvider = MockedProviderBroken as any;
-
-// const apolloClient = jest.requireActual("@apollo/client");
 
 const mocks = [
   {
@@ -42,7 +38,7 @@ const mocks = [
   {
     request: {
       query: GET_POSES,
-      variable: {
+      variables: {
         exerciseIds: []
       }
     },
@@ -50,13 +46,26 @@ const mocks = [
       data: {
         poses: [
           {
-            id: 3,
-            title: "Warrior One pose",
-            subtitle: "Woman demonstrating warrior pose",
-            image_url:
-              "https://t20598560.p.clickup-attachments.com/t20598560/1ec3560e-0f56-409d-848f-54325e5e5be7/DownwardFacingDogAdhoMukahSvanasana_annotated2-918a8568b3af49d2add6d1949797a20f.webp",
-            __typename: "poses",
-          },
+            "id": 3,
+            "title": "War. War never changes. Neither does the classic Warrior One pose!",
+            "subtitle": "Woman demonstrating warrior pose",
+            "image_url": "https://t20598560.p.clickup-attachments.com/t20598560/1ec3560e-0f56-409d-848f-54325e5e5be7/DownwardFacingDogAdhoMukahSvanasana_annotated2-918a8568b3af49d2add6d1949797a20f.webp",
+            "__typename": "poses"
+        },
+        {
+            "id": 2,
+            "title": "Get sturdy with the Mountain pose!",
+            "subtitle": "Woman standing up straight with arms at side and palms facing inner-thighs",
+            "image_url": "https://res.cloudinary.com/dyyp5mhcx/image/upload/v1686001238/Verywell-01-3567198-WarriorOne-acb9d35e634e4f548c2d251e9c739c74_1_tco3sx.webp",
+            "__typename": "poses"
+        },
+        {
+          "id": 1,
+          "title": "Downward Dog",
+          "subtitle": "Adho Mukha Svanasana",
+          "image_url": "https://res.cloudinary.com/dyyp5mhcx/image/upload/v1686001240/DownwardFacingDogAdhoMukahSvanasana_annotated2-918a8568b3af49d2add6d1949797a20f_1_j3t2br.webp",
+          "__typename": "poses"
+      },
         ],
       },
     },
@@ -64,6 +73,9 @@ const mocks = [
   {
     request: {
       query: GET_FAVORITE_POSES,
+      variable: {
+        user_id: null
+      }
     },
     result: {
       data: {
@@ -92,6 +104,17 @@ jest.mock("@auth0/nextjs-auth0/client", () => ({
   }),
 }));
 
+jest.mock("next/router", () => ({
+  useRouter() {
+      return {
+          route: "/",
+          pathname: "",
+          query: "",
+          asPath: "",
+      };
+  },
+}));
+
 describe("Yoga app", () => {
   it("filters yoga poses", async () => {
     const page = render(
@@ -101,14 +124,22 @@ describe("Yoga app", () => {
     );
 
     await page.findByText("Filters");
-    // await page.findByText("Warrior One pose"); // climbing pose
+    await page.findByText("Downward Dog"); // running pose
 
-    // const runningCheckbox = page.getByTestId("checkbox-running");
+    const runningCheckbox = page.getByTestId("checkbox-running");
     // const swimmingCheckbox = page.getByTestId("checkbox-swimming");
-    const climbingCheckbox = page.getByTestId("checkbox-climbing");
+    // const climbingCheckbox = page.getByTestId("checkbox-climbing");
 
-    fireEvent.change(climbingCheckbox, { target: { value: "0" } });
+    // fireEvent(
+    //   runningCheckbox,
+    //   new MouseEvent('click', {
+    //     bubbles: true,
+    //     cancelable: true,
+    //   }),
+    // )
 
-    // expect(page.getByText("Warrior One pose")).toBeNull(); // climbing pose
+    fireEvent.change(runningCheckbox, { target: { checked: false } });
+
+    expect(page.getByText("Downward Dog")).toBeNull(); // this pose should be filtered out
   });
 });
