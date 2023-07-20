2faebad2752317b6c40b6ea4385d7154f1a35d3f
Adrian Ross <adriantoddross@gmail.com>
2023-07-20 15:25:51 -0700

    Find filters text

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,44 +1,83 @@
-import { render } from "@testing-library/react";
-import Home from "../pages";
 import { MockedProvider as MockedProviderBroken } from "@apollo/client/testing";
-// import { gql } from "@apollo/client";
+import { fireEvent, render } from "@testing-library/react";
+import GET_EXERCISES from "lib/gql/queryDefs/getExercises";
+import GET_FAVORITE_POSES from "lib/gql/queryDefs/getFavoritePoses";
+import GET_POSES from "lib/gql/queryDefs/getPoses";
+import Home from "../pages";
 // import { UserProvider } from "@auth0/nextjs-auth0/client";
 // import { Exercise } from "lib/typeDefs/types";
 
 const MockedProvider = MockedProviderBroken as any;
 
-const apolloClient = jest.requireActual("@apollo/client");
+// const apolloClient = jest.requireActual("@apollo/client");
 
-jest.mock("@apollo/client", () => ({
-  ...apolloClient,
-  useQuery: (queryName: string, variables: { exerciseIds?: number[] }) => {
-    if (variables.exerciseIds) {
-      return {
-        data: [
+const mocks = [
+  {
+    request: {
+      query: GET_EXERCISES,
+    },
+    result: {
+      data: {
+        exercise: [
           {
             id: 1,
             title: "running",
             __typename: "exercise",
           },
-        ],
-      };
-    } else {
-      return {
-        data: [
+          {
+            id: 2,
+            title: "climbing",
+            __typename: "exercise",
+          },
           {
             id: 3,
-            title:
-              "War. War never changes. Neither does the classic Warrior One pose!",
+            title: "swimming",
+            __typename: "exercise",
+          },
+        ],
+      },
+    },
+  },
+
+  {
+    request: {
+      query: GET_POSES,
+      variable: {
+        exerciseIds: []
+      }
+    },
+    result: {
+      data: {
+        poses: [
+          {
+            id: 3,
+            title: "Warrior One pose",
             subtitle: "Woman demonstrating warrior pose",
             image_url:
               "https://t20598560.p.clickup-attachments.com/t20598560/1ec3560e-0f56-409d-848f-54325e5e5be7/DownwardFacingDogAdhoMukahSvanasana_annotated2-918a8568b3af49d2add6d1949797a20f.webp",
             __typename: "poses",
           },
         ],
-      };
-    }
+      },
+    },
   },
-}));
+  {
+    request: {
+      query: GET_FAVORITE_POSES,
+    },
+    result: {
+      data: {
+        user_pose: [
+          {
+            id: 63,
+            pose_id: 2,
+            __typename: "user_pose",
+          },
+        ],
+      },
+    },
+  },
+];
 
 jest.mock("@auth0/nextjs-auth0/client", () => ({
   UserProvider: ({ children }) => "<div>{children}</div>",
@@ -56,13 +95,20 @@ jest.mock("@auth0/nextjs-auth0/client", () => ({
 describe("Yoga app", () => {
   it("filters yoga poses", async () => {
     const page = render(
-      <MockedProvider mocks={[]}>
+      <MockedProvider mocks={mocks}>
         <Home />
       </MockedProvider>
     );
 
-    // expect(page.getByText("Filters")).toBeInTheDocument();
     await page.findByText("Filters");
-    expect(page).toMatchSnapshot();
+    // await page.findByText("Warrior One pose"); // climbing pose
+
+    // const runningCheckbox = page.getByTestId("checkbox-running");
+    // const swimmingCheckbox = page.getByTestId("checkbox-swimming");
+    const climbingCheckbox = page.getByTestId("checkbox-climbing");
+
+    fireEvent.change(climbingCheckbox, { target: { value: "0" } });
+
+    // expect(page.getByText("Warrior One pose")).toBeNull(); // climbing pose
   });
 });
